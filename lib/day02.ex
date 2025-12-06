4e1ee996faf159e44b5f5bfb2c96272908d76900
Madison Sites <madison.sites@gmail.com>
2025-12-06 11:53:53 -0800

    Day 2 -Use stream to prevent timeouts from in-memory methods

--- lib/day02.ex
+++ lib/day02.ex
@@ -2,26 +2,21 @@ defmodule Day02 do
   def solve(input) do
     output =input
     |> Enum.map(&parse_line/1)
-    |> Enum.flat_map(&output_range/1)
-    |> Enum.filter(&invalid_sku?/1)
+    |> Stream.flat_map(fn {start_id, end_id} ->
+      start_id..end_id
+    end)
+    |> Stream.filter(&invalid_sku?/1)
     |> Enum.sum()
 
-    # IO.inspect(output, label: "output")
+    IO.inspect(output, label: "output")
     output
   end
 
   def parse_line(line) do
-    IO.inspect(line, label: "parse_line_input")
+    # IO.inspect(line, label: "parse_line_input")
     [first_id, second_id] = String.split(line, "-")
     output = {String.to_integer(first_id), String.to_integer(second_id)}
-    IO.inspect(output, label: "parse_line_output")
-    output
-  end
-
-  def output_range({start_id, end_id}) do
-    IO.inspect({start_id, end_id}, label: "output_range_input")
-    output = Enum.to_list(start_id..end_id)
-    IO.inspect(output, label: "output_range_output", as: :list)
+    # IO.inspect(output, label: "parse_line_output")
     output
   end
 
@@ -32,11 +27,11 @@ defmodule Day02 do
 
     if even_length do
       halfway_point = (sku_length / 2) |> floor()
-      IO.inspect(sku_string, label: "sku_string")
-      IO.inspect(halfway_point, label: "halfway_point")
+      # IO.inspect(sku_string, label: "sku_string")
+      # IO.inspect(halfway_point, label: "halfway_point")
       {first_half, second_half} = String.split_at(sku_string, halfway_point)
-      IO.inspect(first_half, label: "first_half")
-      IO.inspect(second_half, label: "second_half")
+      # IO.inspect(first_half, label: "first_half")
+      # IO.inspect(second_half, label: "second_half")
       first_half == second_half
     else
       false
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -11,7 +11,7 @@ defmodule Day02Test do
 
   test "solves the puzzle" do
     assert Day02.solve(@short_input) == 1227775554
-    assert Day02.solve(@long_input) == 2
+    assert Day02.solve(@long_input) == 26255179562
   end
 
   test "parse_line parses the input" do
@@ -23,17 +23,6 @@ defmodule Day02Test do
     assert Day02.parse_line("1698522-1698528") == {1698522, 1698528}
     assert Day02.parse_line("446443-446449") == {446443, 446449}
     assert Day02.parse_line("38593856-38593862") == {38593856, 38593862}
-  end
-
-  test "output_range outputs the range" do
-    assert Day02.output_range({11, 22}) == [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
-    assert Day02.output_range({95, 115}) == [95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]
-    assert Day02.output_range({998, 1012}) == [998, 999, 1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012]
-    assert Day02.output_range({1188511880, 1188511890}) == [1188511880, 1188511881, 1188511882, 1188511883, 1188511884, 1188511885, 1188511886, 1188511887, 1188511888, 1188511889, 1188511890]
-    assert Day02.output_range({222220, 222224}) == [222220, 222221, 222222, 222223, 222224]
-    assert Day02.output_range({1698522, 1698528}) == [1698522, 1698523, 1698524, 1698525, 1698526, 1698527, 1698528]
-    assert Day02.output_range({446443, 446449}) == [446443, 446444, 446445, 446446, 446447, 446448, 446449]
-    assert Day02.output_range({38593856, 38593862}) == [38593856, 38593857, 38593858, 38593859, 38593860, 38593861, 38593862]
   end
 
   test "invalid_sku? returns true for repeated digit SKUs" do
